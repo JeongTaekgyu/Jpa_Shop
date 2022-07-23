a04d50609e2089f991e6f219b88f5e511eb96723
taekgyu <taekgyu0602@gmail.com>
2022-07-23 20:10:36 +0900

    상품 Service 구현

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기에서 (readOnly = true)는 jpa가 조회하는 곳에서는 성능을 최적화 한다
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // 쓰기는 readOnly = true하면 안된다. default는 false이다.
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
