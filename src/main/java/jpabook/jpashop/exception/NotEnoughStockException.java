9b6f11f3ab13e3dff9b63b6ba311be94f572c28c
taekgyu <taekgyu0602@gmail.com>
2022-07-23 19:57:06 +0900

    상품 엔티티에 비즈니스 로직 추가

---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException{
+
+    // alt + Insert 로 Override Method 했다.
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NotEnoughStockException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
