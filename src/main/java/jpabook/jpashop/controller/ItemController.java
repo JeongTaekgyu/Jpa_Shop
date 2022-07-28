36eb1b26a511784a923619c897113ac767f001b2
taekgyu <taekgyu0602@gmail.com>
2022-07-28 17:05:01 +0900

    상품 등록 구현

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        Book book = new Book();
+
+        // setter 제거하고 create메서드 같은거 하는게 더 좋은 코드이다. 하지만 가벼운 예제 하려고 setter 사용한다.
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+}
